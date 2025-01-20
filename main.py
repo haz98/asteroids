62c2230a4065ea07584dab7d20fa6f59661983dd
haz98 <hajnikzdenko98@gmail.com>
2025-01-20 22:34:30 +0100

    added shooting

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
-
+from shot import Shot
 
 def main():
     pygame.init()
@@ -13,10 +13,12 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
+    Shot.containers = (shots, drawable, updatable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     asteroidfield = AsteroidField()
@@ -37,6 +39,11 @@ def main():
             if object.collision(player):
                 print("Game over!")
                 return
+            for shot in shots:
+                if object.collision(shot):
+                    object.kill()
+                    shot.kill()
+        
         pygame.display.flip()
 
         # limit the framerate to 60 FPS
