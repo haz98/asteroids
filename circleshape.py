56e63dc256f9609ca7d720181a2efa216b8fc4d0
haz98 <hajnikzdenko98@gmail.com>
2025-01-20 21:59:46 +0100

    made usafe of containters + fixed circleshape

--- circleshape.py
+++ circleshape.py
@@ -8,9 +8,9 @@ class CircleShape(pygame.sprite.Sprite):
                         super().__init__(self.containers)
                 else:
                         super().__init__()
-                        self.position = pygame.Vector2(x, y)
-                        self.velocity = pygame.Vector2(0, 0)
-                        self.radius = radius
+                self.position = pygame.Vector2(x, y)
+                self.velocity = pygame.Vector2(0, 0)
+                self.radius = radius
 
         def draw(self, screen):
         # sub-classes must override
--- main.py
+++ main.py
@@ -7,7 +7,14 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     dt = 0
 
     while True:
@@ -16,8 +23,10 @@ def main():
                 return
 
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        for object in updatable:
+            object.update(dt)
+        for object in drawable:
+            object.draw(screen)
         pygame.display.flip()
 
         # limit the framerate to 60 FPS
