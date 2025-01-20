0c0a8f07adf5fb0082741a16aad2520cb97df01c
haz98 <hajnikzdenko98@gmail.com>
2025-01-20 22:14:41 +0100

    added collision

--- circleshape.py
+++ circleshape.py
@@ -19,3 +19,7 @@ class CircleShape(pygame.sprite.Sprite):
         def update(self, dt):
         # sub-classes must override
                 pass
+        def collision(self,circle):
+                if self.position.distance_to(circle.position) < self.radius + circle.radius:
+                        return True
+                return False
